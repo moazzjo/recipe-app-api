979588410052eb1e851c8a2b7a0fa2902d2af6d6
mouazjo <moazzjom@icloud.com>
2024-08-27 11:46:47 +0300

    config Database postgresql with docker

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+  For test exercise
+
+"""
+
+
+def add(x,y):
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+simple test
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class TestCalc(SimpleTestCase):
+
+    def testAdd(self):
+        res = calc.add(3, 7)
+
+        self.assertEqual(res,10)
